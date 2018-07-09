6330784b058e5d536c3a9a99b99dc709b7207d21
banyan Cheung <i@zhangrong.cc>
2018-07-09 18:21:37 +0800

    init dirs

---
+++ manage.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pylog',
+    version='1.0.0',
+    author='banyan cheung',
+    author_email='i@zhangrong.cc',
+    description="pylog is simple blog written by python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/banyancheung/pyblog",
+    packages=find_packages,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask', 'Flask-WTF', 'Flask-Script', 'PyMySQL'
+    ],
+)
--- pylog/__init__.py
+++ pylog/__init__.py
@@ -7,7 +7,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-
     )
 
     try:
